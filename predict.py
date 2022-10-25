d312f076e3eeb1b08cb1129de7749038d91f746b
Varsha075 <94888759+Varsha075@users.noreply.github.com>
2022-10-25 22:56:56 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -11,9 +11,13 @@ img2 = img / 255.0
 result = net([img2]).numpy()[0]
 coord = result[0:8]
 coord = [int(x * 224) for x in coord]
-cv2.circle(img_show, (coord[0], coord[1]), 3, (0, 0, 255),-1)
-cv2.circle(img_show, (coord[2], coord[3]), 3, (0, 255, 255),-1)
+cv2.circle(img_show, (coord[0], coord[1]), 3, (255, 0, 0),-1)
+cv2.circle(img_show, (coord[2], coord[3]), 3, (255, 0, 0),-1)
 cv2.circle(img_show, (coord[4], coord[5]), 3, (255, 0, 0),-1)
-cv2.circle(img_show, (coord[6], coord[7]), 3, (0, 255, 0),-1)
+cv2.circle(img_show, (coord[6], coord[7]), 3, (255, 0, 0),-1)
+cv2.line(img_show,(coord[0], coord[1]),(coord[2], coord[3]),2,(0, 0, 255))
+cv2.line(img_show,(coord[2], coord[3]),(coord[4], coord[5]),2,(0, 0, 255))
+cv2.line(img_show,(coord[4], coord[5]),(coord[6], coord[7]),2,(0, 0, 255))
+cv2.line(img_show,(coord[6], coord[7]),(coord[0], coord[1]),2,(0, 0, 255))
 cv2.imshow("show", img_show)
 cv2.waitKey(0)
