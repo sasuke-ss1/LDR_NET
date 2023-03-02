86f8fd48d11ea6024e3d82f08d1292177d6c322c
Varad Bhatkar <95376340+Varad-17@users.noreply.github.com>
2023-03-02 17:19:59 +0530

    updated predict.py for scaling

--- predict.py
+++ predict.py
@@ -2,22 +2,34 @@ import cv2
 import numpy as np
 import torch
 
-
-vid = cv2.VideoCapture("/home/sasuke/repos/LDR_NET/sampleDataset/input_sample/background00/datasheet001.avi")
-net = torch.load("./model1.pth").to(torch.device("cpu")).eval()
+vidpath = 'TestVideo path'
+vid = cv2.VideoCapture(vidpath)
+net = torch.load("./model.pth").to(torch.device("cpu")).eval()
 
 while True:
     ret, frame = vid.read()
+    clone = frame.copy()
     img = cv2.resize(frame, (224, 224))
     img2 = torch.permute(torch.from_numpy((img.astype(np.float32) - 127.5) / 255.0), (2, 0, 1)).unsqueeze(0)
-    coord = net(img2)[0].detach().numpy().reshape((4, 2)) * 224
+    border = net(img2)[1].detach().numpy().reshape((-1,2))
+    coord = net(img2)[0].detach().numpy().reshape((-1, 2))
+    coord[:,0] = coord[:,0]*1920
+    coord[:,1] = coord[:,1]*1080
+    border[:,0] = border[:,0]*1920
+    border[:,1] = border[:,1]*1080
     coord = coord.astype(np.int32)
-    img = cv2.polylines(img, [coord], True, (0, 255, 0), 8)
-    img = cv2.resize(img, (1920, 1080))
-
+    for c in coord:
+        clone = cv2.circle(clone, tuple(c), 10, (255,0, 0), 2)
+    for b in border:
+        print(b)
+        clone = cv2.circle(clone, tuple(b), 10, (255,0,0), 2)
+    img = cv2.polylines(clone, [coord], True, (0, 255, 0), 8)
+    #img = cv2.resize(img, (1920, 1080))
+    
     cv2.imshow("vid", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 vid.release()
 cv2.destroyAllWindows()
+
