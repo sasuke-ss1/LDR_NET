337cc192d1a9e698d0885c8663510e65c5a86ef8
Varad Bhatkar <95376340+Varad-17@users.noreply.github.com>
2023-03-12 00:22:57 +0530

    pakka last wala commit

--- predict.py
+++ predict.py
@@ -2,25 +2,33 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import warnings
-
 warnings.filterwarnings("ignore")
+import os
 
-net = tf.keras.models.load_model("./model_file/model-icdar-1.4")
-vid = cv2.VideoCapture(0)
+net = tf.keras.models.load_model("LDR_NET/model_file/model-icdar-1.4")
+
+vidpath = '/media/web_slinger/Windows/Users/varad/Documents/Computer Vision/On Device DL/testDataset/background03/magazine002.avi'
+vid = cv2.VideoCapture(vidpath)
+
+output = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*"XVID"),30, (1920,1080))
 while True:
     _, frame = vid.read()
     img = frame.copy()
+    x,y,_ = frame.shape
     img = cv2.resize(img, (224, 224))
-    img_show = np.copy(img)
     img = img.astype(np.float32)
     img2 = img / 255.0
     result = net([img2], training=False).numpy()[0]
-    coord = result[0:8]
-    coord = [int(x * 224) for x in coord]
-    cv2.circle(img_show, (coord[0], coord[1]), 3, (0, 0, 255), -1)
-    cv2.circle(img_show, (coord[2], coord[3]), 3, (0, 255, 255), -1)
-    cv2.circle(img_show, (coord[4], coord[5]), 3, (255, 0, 0), -1)
-    cv2.circle(img_show, (coord[6], coord[7]), 3, (0, 255, 0), -1)
-    cv2.imshow("show", img_show)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    coord = np.array(result[0:8]).reshape((-1,2))
+    coord[:,0] = coord[:,0]*y
+    coord[:,1] = coord[:,1]*x
+    coord = coord.astype(np.int32)
+    frame = cv2.polylines(frame , [coord], True, (0, 255, 0), 8)
+    print(x,y)
+    output.write(frame)
+    cv2.imshow("show", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
+
+vid.release()
+output.release()
